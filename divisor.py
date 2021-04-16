0e7647eb4ebe8d3378bceab9c6d28680941a3813
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-04-15 18:56:48 -0700

    find_divisors finds divisors

--- divisor.py
+++ divisor.py
@@ -1,4 +1,11 @@
 from math import sqrt
 
 def find_divisors(n: int):
-    upper_bound = sqrt(n)
+    upper_bound = int(n/2)
+    divisors = {x if n % x == 0 else 0 for x in range(2, upper_bound+1)}
+    divisors.discard(0)
+    print(list(divisors))
+    return divisors
+
+if __name__ == "__main__":
+    find_divisors(int(input("Enter number: ")))
