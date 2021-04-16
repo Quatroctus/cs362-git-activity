545cbbf6febacc727c77cbc9d985b6933300808d
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-04-15 19:00:30 -0700

    calc.py now runs the function when ran.

--- calc.py
+++ calc.py
@@ -3,3 +3,7 @@ def calc(a, b):
     for a in results:
         print(a)
     sum_of_list = sum(results)
+    print(sum_of_list)
+
+if __name__ == "__main__":
+    calc(float(input("Please enter the first number: ")), float(input("Please enter the second number: ")))
