ca5a4176b5b67bb05924460414a255ec0760d7ad
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-04-15 19:33:56 -0700

    gen_password generates a password.

--- password.py
+++ password.py
@@ -1,2 +1,20 @@
+from random import choice, shuffle
+
 def gen_password(n: int):
-    pass
+    digits = "0123456789"
+    symbols = "!@#$%^&*()[]{}\|/?><,.`~"
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()[]{}\|/?><,.`~0123456789"
+    a = []
+    if n > 2:
+        a.append(choice(digits))
+        a.append(choice(symbols))
+        n -= 2
+    for _ in range(n):
+        a.append(choice(alphabet))
+    shuffle(a)
+    p = "".join(a)
+    print(p)
+    return p
+
+if __name__ == "__main__":
+    gen_password(int(input("Please enter password length: ")))
